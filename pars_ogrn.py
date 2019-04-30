d31a2524d4475bd3599c1208c546e172a28d7075
Khanze99 <khamidov_anvar@mail.ru>
2019-04-30 10:19:09 +0300

    name_company and institution

--- pars_ogrn.py
+++ pars_ogrn.py
@@ -4,7 +4,7 @@ import csv
 
 DICT_NAME_COMPANY = {}
 BASE_URL = 'https://огрн.онлайн/{}'
-API_TOKEN = '....'
+API_TOKEN = '...'
 headers = {'X-ACCESS-KEY': API_TOKEN}
 DATA = []  #COMPANY_NAME,REGION,DATA,INSTITUTION_COMPANY'.split(',')
 DATA_FINANCE = []  #'COMPANY_NAME,FINANCE'.split(',')
@@ -14,7 +14,7 @@ def get_inn_id(search):  # Поиск компаний по инн
     url = BASE_URL.format('интеграция/компании/')
     params = {'инн': search}
     get_id = requests.get(url, timeout=(5, 2), headers=headers, params=params,)  # заддержка т.к. >10 запросов в 1 сек, блокировка
-    print(get_id.json()[0]['id'])
+    print(get_id.json())
     return get_id.json()[0]['id']
 
 
@@ -53,25 +53,31 @@ def get_institution(id):  # Поиск учредителя компании
     params = {'id': id}
     url = BASE_URL.format('интеграция/компании/{}/учредители/').format(id)
     get_instit = requests.get(url, timeout=(5, 1), headers=headers, params=params)
-    print(get_instit.json())
-    print('personOwner' in get_instit.json()[0])
-    print('company' in get_instit.json()[0])
+    # print(get_instit.json())
+    # print('personOwner' in get_instit.json()[0])
+    # print('company' in get_instit.json()[0])
 
     if 'personOwner' in get_instit.json()[0]:
+        print(get_instit.json()[0])
         first_name = get_instit.json()[0]['personOwner']['firstName']
         middle_name = get_instit.json()[0]['personOwner']['middleName']
         sur_name = get_instit.json()[0]['personOwner']['surName']
-        inn_inst = get_instit.json()[0]['inn']
+        inn_inst = get_instit.json()[0]['personOwner']['inn']
         name_inst = '{} {} {}'.format(first_name, middle_name, sur_name)
         name = [name_inst, inn_inst]
-        print(name)
+        # print(name)
         return name
     elif 'company' in get_instit.json()[0]:
         company_name = get_instit.json()[0]['company']['name']
         inn_company = get_instit.json()[0]['company']['inn']
         name = [company_name, inn_company]
-        print(name)
+        # print(name)
         return name
+    else:
+        company_name = get_instit.json()[0]['name']
+        inn_company = get_instit.json()[0]['inn']
+        name = [company_name, inn_company]
+
 
 
 # def get_postname(id):  # Поиск компаний где работает учредитель
@@ -83,23 +89,30 @@ def get_institution(id):  # Поиск учредителя компании
 
 if __name__ == '__main__':
     with open('100.txt', 'r', encoding='utf-8') as file:
-        read = file.readlines()
-        for line in read:
-            inn = int(line)
-            print(inn)
-            name_company = get_id_company(get_inn_id(str(inn)))
-            name_inst = get_institution(get_inn_id(str(inn)))
-            print(name_company)
-            print(name_inst)
-            list_company = name_company.extend(name_inst)
-            # list_company.extend(get_comp_finance(get_inn_id(str(line.split()))[2:-2]))
-            DATA.append(list_company)
+        read = file.read().split('\n')
 
-            with open('first_100.csv', 'a', encoding='utf-8') as file_write:
-                writer = csv.writer(file_write, delimiter=',')
-                for line in DATA:
-                    print(line)
-                    writer.writerow(line)
+        for line in read:
+            print(line)
+            inn = str(line.rstrip('\n'))
+            print(inn)
+            try:
+                name_company = get_id_company(get_inn_id(str(inn)))
+                name_inst = get_institution(get_inn_id(str(inn)))
+            # print(name_company)
+            # print(name_inst)
+                name_company.extend(name_inst)
+                DATA.append(name_company)
+                print(name_company)
+            except BaseException:
+                continue
+            # list_company.extend(get_comp_finance(get_inn_id(str(line.split()))[2:-2]))
+            # print(DATA)
+
+with open('first_100.csv', 'a', encoding='utf-8') as file_write:
+        writer = csv.writer(file_write, delimiter=',')
+        for line in DATA:
+            print(line)
+            writer.writerow(line)
             # with open('finance.csv', 'a', encoding='utf-8') as finance_write:
             #     writer = csv.writer(finance_write, delimiter='\n')
             #     for line in DATA_FINANCE:
